b52553567b52b8913aca14331320f5cb5d741c0d
Teoh Kheng Hong <kh.teoh@maybank.com>
2023-04-04 00:27:17 +0800

    Ionic v7 starter tutorial in progress - Deploying to iOS and Android

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { PhotoService } from '../services/photo.service';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent, NgForOf]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) { }
 
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery().then(() => {
+      console.log('this.photoService.photos: ', this.photoService.photos);
+    });
+  }
 }
